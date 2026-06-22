ac8f9300002a989a42034dda68b133e70aa56bf8
Gali Thanvika <145558457+Thanvika-gali@users.noreply.github.com>
2026-06-22 20:26:51 +0530

    Implement Singleton pattern in Logger class

--- Engineering Concepts/Exercise 1 Implementing the Singleton Pattern/Logger.java
+++ Engineering Concepts/Exercise 1 Implementing the Singleton Pattern/Logger.java
@@ -1,1 +1,21 @@
+public class Logger {
 
+    private static Logger logger;
+
+    private Logger() {
+        System.out.println("Logger object created successfully.");
+    }
+
+    public static Logger getLogger() {
+
+        if (logger == null) {
+            logger = new Logger();
+        }
+
+        return logger;
+    }
+
+    public void writeLog(String message) {
+        System.out.println("[LOG] " + message);
+    }
+}
